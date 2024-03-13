955811d844fe325396083f5ff1c1677fd4cfe4a7
NinjaCoder95 <108400828+NinjaCoder95@users.noreply.github.com>
2024-03-13 13:40:40 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [apiState, setApiState] = useState([]);
+  const [searchType, setSearchType] = useState('firstName');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
+  const apiCall = async () => {
+    const response = await fetch('https://jsonplaceholder.org/users');
+    const responseValue = await response.json();
+    setApiState(responseValue);
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredUsers = apiState.filter(person => {
+    if (searchType === 'firstName') {
+      return person.firstname.toLowerCase().includes(searchQuery.toLowerCase());
+    } else {
+      return person.lastname.toLowerCase().includes(searchQuery.toLowerCase());
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>api</h1>
+      <select value={searchType} onChange={e => setSearchType(e.target.value)}>
+        <option value="firstname">First Name</option>
+        <option value="lastname">Last Name</option>
+      </select>
+      <input type="text" value={searchQuery} onChange={handleSearchChange} placeholder="Search..." />
+      <ul>
+        {filteredUsers.map(person => (
+          <li key={person.id}>
+            {person.firstname} {person.lastname}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
